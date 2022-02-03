2a25deff821f30e9be6668871f2ac613f722f2e7
LidoKing <kinglido203@gmail.com>
2022-02-03 14:08:35 +0800

    used multer to get all necessary info

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -11,10 +13,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  let uploaded = req.file;
+  console.log(uploaded);
+  res.json({ name: uploaded.originalname, type: uploaded.mimetype, size: uploaded.size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
